7a1c2878a937e25f097ff07b9f4f848bf28568c2
A <[alihankassiman@gamil.com]>
2025-04-04 01:02:06 +0500

    Initial Commit

---
+++ Pinterest/PinterestApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinterestApp.swift
+//  Pinterest
+//
+//  Created by Alikhan Kassiman on 2025.04.04.
+//
+
+import SwiftUI
+
+@main
+struct PinterestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
